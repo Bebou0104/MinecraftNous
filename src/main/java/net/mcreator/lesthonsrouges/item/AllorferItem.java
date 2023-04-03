709931f3ef67ad45632cfea049f8c78903afe8d6
Bebou0104 <bebou@bebou-ordi>
2023-04-03 16:21:37 +0200

    alliage fer et or ameliorer

--- src/main/java/net/mcreator/lesthonsrouges/item/AllorferItem.java
+++ src/main/java/net/mcreator/lesthonsrouges/item/AllorferItem.java
@@ -1,13 +1,19 @@
 
 package net.mcreator.lesthonsrouges.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import javax.annotation.Nullable;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
 
 public class AllorferItem extends Item {
-
 	public AllorferItem() {
-		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(64).rarity(Rarity.COMMON));
 	}
 
 	@Override
@@ -15,5 +21,4 @@ public class AllorferItem extends Item {
 		super.appendHoverText(itemstack, world, list, flag);
 		list.add(Component.literal("\u00E0 utiliser pour rafiner l'uranium"));
 	}
-
 }
--- src/main/java/net/mcreator/lesthonsrouges/item/RafuraItem.java
+++ src/main/java/net/mcreator/lesthonsrouges/item/RafuraItem.java
@@ -1,13 +1,12 @@
 
 package net.mcreator.lesthonsrouges.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import javax.annotation.Nullable;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
 
 public class RafuraItem extends Item {
-
 	public RafuraItem() {
 		super(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(64).fireResistant().rarity(Rarity.COMMON));
 	}
-
 }
